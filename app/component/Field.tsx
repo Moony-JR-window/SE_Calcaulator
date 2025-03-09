45bc8306a661cf639558e6c34f5fb524a7d32673
mony.rorn <mony.rorn@wingbank.com.kh>
2025-03-09 11:54:07 +0700

    delete@1.0.2

--- app/component/Field.tsx
+++ app/component/Field.tsx
@@ -25,7 +25,7 @@ const Field: React.FC<FieldProps> = ({ onTotalChange, reset }) => {
   }, [number, percentage, onTotalChange]);  // Only update when `number` or `percentage` change
 
   return (
-    <div className="flex items-center gap-2 border p-3 rounded-lg shadow-sm bg-white transition duration-300">
+    <div className="flex items-center gap-2 border p-3 rounded-lg shadow-sm transition duration-300">
       <input
         type="text"
         className="w-24 p-2 border rounded-md focus:ring-2 focus:ring-blue-300 bg-white dark:bg-gray-700 text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400 transition duration-300"
